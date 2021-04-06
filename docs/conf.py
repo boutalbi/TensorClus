333f9dea9a1854399f9b425e8d4039e30eedccd7
BoutalbiRafika <boutalbi.rafika@gmail.com>
2021-04-06 13:00:15 +0200

    Some corrections

--- docs/conf.py
+++ docs/conf.py
@@ -398,7 +398,10 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.autodoc']##'sphinx.ext.todo', 'sphinx.ext.viewcode','sphinx.ext.napoleon','sphinx.ext.intersphinx'
+extensions = [ 'sphinx.ext.autodoc','numpydoc',    'sphinx.ext.mathjax','sphinx.ext.viewcode',
+    'numpydoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',]##'sphinx.ext.todo', 'sphinx.ext.viewcode','sphinx.ext.napoleon','sphinx.ext.intersphinx'
 napoleon_use_param = False
 #napoleon_use_ivar  = False
 #napoleon_numpy_docstring = True
